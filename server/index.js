11127144e5ba3bcfdfc82587821f86cc28c40928
joshua marieta <josmarieta@gmail.com>
2019-11-14 12:11:08 +0300

    Changed the localhost

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const monk = require('monk');
 
 const app = express();
-const db = monk('localhost:5000/meower');
+const db = monk('http://localhost:5000/meower');
 const mews = db.get('mews');
 
 app.use(cors());
@@ -27,13 +27,13 @@ app.get('/mews',(req,res)=>{
 function isValidMew(mew){
   return mew.name && mew.name.toString().trim() !== ''&&
   mew.content && mew.content.toString().trim() !== '';
-}
+};
 
 app.post('/mews',(req,res)=>{
  if(isValidMew(req.body)){
    const mew = {
      name: req.body.name.toString(),
-     content:req.body.content.toString(),
+     content: req.body.content.toString(),
      created: new Date()
    };
    mews
